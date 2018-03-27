3073d8a85991dee73443c72f86bbabd988c639c7
Deepak Goyal <dkkumargoyal@gmail.com>
2018-03-27 19:43:56 +0530

    golang build fix

--- clean.sh
+++ clean.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 declare -a folders=("csharp1" "csharp2" "java" "python2" "python3" "golang" "nodejs6" "nodejs4")
 
-#export AWS_PROFILE=personal
-
-  do
   for folder in "${folders[@]}"
   do
     cd $folder
